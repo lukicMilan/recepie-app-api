17d469fda4343d0c8ee1ec7514bb82d6dfb20fed
lukicMilan <lukic.milan22@gmail.com>
2023-06-01 14:17:05 +0200

    Add value error for missing email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,3 +35,8 @@ class ModelTests(TestCase):
                 email, "password_sample"
             )
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "testPassword")
